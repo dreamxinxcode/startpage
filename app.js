9fee6ab2c1988f59517f9fbaf9d940463b0c4992
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-20 01:36:08 -0700

    added google search

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const axios = require('axios');
-
-const KEY = 'e0d88f84620009644667c282466ed877';
-axios.get(`api.openweathermap.org/data/2.5/weather?q=${Victoria}&appid=${KEY}`)
-.then(res => console.log(res))
+// axios.get(`api.openweathermap.org/data/2.5/weather?q=${Victoria}&appid=${KEY}`)
+// .then(res => console.log(res))
 
 const bookmark_list = [
-  {title: 'title', category: 'category', url: 'www.google.com'},
+  {title: 'GitHub', category: 'category', url: 'https://github.com/dreamxinxcode'},
   {title: 'title', category: 'category', url: 'www.google.com'},
   {title: 'title', category: 'category', url: 'www.google.com'}
 ];
 
 const bookmarks = bookmark_list.map(bookmark => {
-  return `<a href='${bookmark.url}'>${bookmark.title}</a>`
+  return `<li><a href='${bookmark.url}'>${bookmark.title}</a></li>`
 });
 
-document.getElementById('test').innerHTML = bookmarks;
+document.getElementById('test-ul').innerHTML = bookmarks;
+
+const updateClock = () => {
+  const now = new Date();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const time = `${now.getHours()} : ${now.getMinutes()}`;
+
+  document.getElementById('time').innerHTML = time;
+
+  setTimeout(updateClock, 1000);
+};
+
+updateClock();
