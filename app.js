269d704e57c36bd3c8ed30838f87fe4a20076cf1
dreamxinxcode <dreamxinxcode@gmail.com>
2020-10-18 04:25:38 -0700

    starting on geolocation

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-// Open Weather Map API
-fetch(
-  `http://api.openweathermap.org/data/2.5/weather?q=Victoria&appid=${API_KEY}`
-)
-  .then(function (response) {
-    if (response.status !== 200) {
-      console.log("Error:" + response.status);
-      return;
-    }
+let coords = {
+  latitude: "",
+  longitude: "",
+};
 
-    response.json().then(function (data) {});
+const fetchIP = async () => {
+  const resopnse = await fetch("https://api.ipify.org?format=json");
+  const data = await resopnse.json();
+  return data;
+};
+
+console.log(fetchIP());
+const getLocation = (callback) => {
+  // let promise = new Promise((resolve, reject) => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       latitude = position.coords.latitude;
+  //       longitude = position.coords.longitude;
+  //     });
+  //   }
+  // });
+};
+
+fetch(
+  `http://api.openweathermap.org/data/2.5/weather?q=Victoria&appid=e0d88f84620009644667c282466ed877`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    getLocation(() => console.log("lat", latitude, "lon", longitude));
+    console.log(data);
   })
   .catch(function (err) {
     console.log("Fetch Error :-S", err);
